c4ae5d43155506ec83f101a1b3c61518884a7bb7
Walcygleicson <113854464+Walcygleicson@users.noreply.github.com>
2022-10-24 00:19:49 -0300

    Initial commit

---
+++ Módulo 2/ex21 CSS Pseudo Class/a.py
@@ -0,0 +1,14 @@
+
+while True:
+    joao = input('Digite a idade de João: ')
+    pedro = input('Digite a idade de Pedro: ')
+
+
+    if joao > pedro:
+        print('JÃO É MAIS VELHO QUE PEDRO')
+    elif joao < pedro:
+        print('JÃO É MAIS NOVO QUE PEDRO')
+    else:
+        print('JOÃO E PEDRO TEM A MESMA IDADE')
+    break
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# HTML_CSS_Aulas
+ Aulas de HTM5 e CSS3 do professor Gustavo Guanabara
